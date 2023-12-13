bd72d4a322081d35d0fd8a664b88941e622022e2
jumpei yamada <jumpei_yamada@mates-system.com>
2023-12-13 14:19:09 +0900

    add: ownership: string

--- src/ownership/string.rs
+++ src/ownership/string.rs
@@ -8,4 +8,21 @@ pub fn main() {
   let mut s = String::from("hello");
   s.push_str(", world!");
   println!("{}", s);
+
+
+  /*
+   * NOTE: s1をs2に代入すると、String型のデータがコピーされる。
+   * つまり、スタックにあるポインタ、長さ、 許容量をコピーするということで、ポインタが指すヒープ上のデータはコピーしない
+   * -> jsのオブジェクトの参照渡しと同じ??
+   */
+  let s1 = String::from("hello");
+  let _s2 = s1;
+  // println!("{}, world!", s1); // error: value borrowed here after move
+
+  /*
+   * NOTE: clone()を使うと、ヒープ上のデータもコピーされる
+   */
+  let s3 = String::from("hello");
+  let s4 = s3.clone();
+  println!("s3 = {}, s4 = {}", s3, s4);
 }
