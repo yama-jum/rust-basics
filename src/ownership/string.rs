ea43eef28ee99818a046013b36c2dc78dadf75c0
pei-pay <gleektennis@icloud.com>
2024-01-14 11:04:50 +0900

    tweak: adding comment

--- src/ownership/string.rs
+++ src/ownership/string.rs
@@ -1,14 +1,13 @@
 pub fn main() {
   // String Literal Type: immutable
   let s = "hello";
-  // s.push_str(", world!"); 
+  // s.push_str(", world!");
   println!("{}", s);
 
   // String Type: mutable
   let mut s = String::from("hello");
   s.push_str(", world!");
   println!("{}", s);
-
 
   /*
    * NOTE: s1をs2に代入すると、String型のデータがコピーされる。
