408f181531d654414a04a18242d18655206f8c31
pei-pay <gleektennis@icloud.com>
2024-02-11 17:23:31 +0900

    wip: ownership: slice

--- src/ownership/mod.rs
+++ src/ownership/mod.rs
@@ -1,6 +1,7 @@
 mod function;
-mod references_and_borrowing;
 mod string;
+mod references_and_borrowing;
+mod slice;
 
 pub fn main() {
   println!("==string==");
@@ -8,5 +9,7 @@ pub fn main() {
   println!("==function==");
   function::main();
   println!("==references and borrowing==");
-  references_and_borrowing::main()
+  references_and_borrowing::main();
+  println!("===slice==");
+  slice::main();
 }
